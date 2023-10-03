a051c91c4c9ba92e460040e32b16cd9b04371a97
Gachenge <gachenge1@gmail.com>
2023-10-03 16:53:12 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+STATIC_FOLDER = os.path.join(os.getcwd(), "static")
+
+QUEUE_NAME = "transcription_tasks"
+
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
+RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD')
