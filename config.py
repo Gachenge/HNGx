6d80927f84b8ad17f242b6856683918a25a5e9b3
Gachenge <gachenge1@gmail.com>
2023-10-03 16:37:00 +0300

    config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-STATIC_FOLDER = os.path.join(os.getcwd(), "static")
-
-QUEUE_NAME = "transcription_tasks"
-
-RABBITMQ_HOST = "lionfish-01.rmq.cloudamqp.com/gctzytvi"
-RABBITMQ_USER = "gctzytvi"
-RABBITMQ_PASSWORD = "HuFMOqQqJpNjBqk8tiktUOENOgYdgT59"
